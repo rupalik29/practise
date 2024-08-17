1700ec51c7de390b62adddf07d39e92403474a38
varshaadake <varshaadake3014@gmail.com>
2024-08-17 23:24:16 +0530

    [varsha] adding new lines

--- src/practise/Methods.java
+++ src/practise/Methods.java
@@ -3,7 +3,10 @@ package practise;
 public class Methods {
 
 	public static void main(String[] args) {
+		
 		System.out.println("Welcome to Java");
+		System.out.println("welcome to selenium");
+		
 	}
 
 }
